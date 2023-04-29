008eff9926d2b4a3f000eeda0a1e1812b53bc3b7
James-Hicks <james8hicks@gmail.com>
2023-04-29 16:28:12 -0400

    feat: order spawning progress

--- LudumDare53/Assets/LD53/Scripts/GameManager.cs
+++ LudumDare53/Assets/LD53/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     [Range(0, 10)][SerializeField] private int minItems;
     [Range(0, 10)][SerializeField] private int maxItems;
     [SerializeField] private GameObject[] boxes;
+    [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject[] usedSpawnPoints;
     public Order currentOrder;
 
     private void Awake()
@@ -16,27 +18,74 @@ public class GameManager : MonoBehaviour
         CreateOrder();
     }
 
+    private void Update()
+    {
+        
+    }
+
     public void CreateOrder()
     {
+        Debug.Log("Creating Order");
         Order newOrder = new Order();
         int orderLength = Random.Range(minItems, maxItems);
         for(int i = 0; i < orderLength; i++)
         {
-            Debug.Log($"Box {i+1}/{orderLength}");
-        }
 
+            newOrder.OrderRequirements.Add(boxes[Random.Range(0, boxes.Length)]);
+        }
         currentOrder = newOrder;
         StartNewOrder();
     }
 
     private void StartNewOrder()
     {
-        throw new System.NotImplementedException();
+        for(int i = 0; i < currentOrder.OrderRequirements.Count; i++)
+        {
+            Debug.Log("Starting Order");
+            bool goodSpawnPoint = true;
+            GameObject spawnPoint;
+
+            // Verify Spawn Point
+            do
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                for(int b = 0; b< usedSpawnPoints.Length; b++)
+                {
+                    if (usedSpawnPoints[i] == spawnPoint)
+                    {
+                        goodSpawnPoint = false;
+                        break;
+                    }
+                }
+            } while (!goodSpawnPoint);
+
+            for (int b = 0; b < usedSpawnPoints.Length; b++)
+            {
+                if (usedSpawnPoints[b] == null)
+                {
+                    usedSpawnPoints[b] = spawnPoint;
+                }
+            }
+
+            GameObject spawnedBox = Instantiate(currentOrder.OrderRequirements[i], spawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void EndOrder()
+    {
+        Debug.Log("Finished Order");
+        for (int i = 0; i < usedSpawnPoints.Length; i++)
+        {
+            usedSpawnPoints[i] = null;
+        }
+
+        CreateOrder();
     }
 }
 
 public class Order
 {
     public List<GameObject> OrderRequirements;
+    public bool metRequirements = false;
 
 }
