c0b0adc86f25b0a896bb61099516c61e83f7280b
James-Hicks <james8hicks@gmail.com>
2023-04-28 23:21:07 -0400

    feat(player): tank controls done

--- LudumDare53/Assets/LD53/Scripts/PlayerControl.cs
+++ LudumDare53/Assets/LD53/Scripts/PlayerControl.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
-    public GameObject Lift;
+    [SerializeField] private GameObject Lift;
+    [SerializeField] private float playerSpeed = 3f;
+    [SerializeField] private float rotationSpeed = 40f;
+
+    private Vector2 playerInput;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) Cursor.lockState = CursorLockMode.Locked;
-        else if (Input.GetKeyDown(KeyCode.Mouse1)) Cursor.lockState = CursorLockMode.None;
-
-
-        if(Input.GetKey(KeyCode.Q))
-        {
-            Debug.Log("Raise Lift");
-        }
-        
-        if (Input.GetKey(KeyCode.E)) 
-        {
-            Debug.Log("Lower Lift");
-        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1)) Cursor.lockState = CursorLockMode.None; 
     }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * playerInput.y * playerSpeed * Time.deltaTime, ForceMode.Impulse);
+        transform.Rotate(transform.up, rotationSpeed * playerInput.x * Time.deltaTime);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        playerInput = value.Get<Vector2>();
+    }
+
+
 }
