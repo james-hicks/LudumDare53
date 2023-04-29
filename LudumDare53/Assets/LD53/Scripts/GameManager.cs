c1d604aa304f357d07fb5336c70e841d7d2af9dc
James-Hicks <james8hicks@gmail.com>
2023-04-29 15:40:18 -0400

    feat: game manager progress

--- LudumDare53/Assets/LD53/Scripts/GameManager.cs
+++ LudumDare53/Assets/LD53/Scripts/GameManager.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPrefab;
-    void Update()
+
+    [Range(0, 10)][SerializeField] private int minItems;
+    [Range(0, 10)][SerializeField] private int maxItems;
+    [SerializeField] private GameObject[] boxes;
+    public Order currentOrder;
+
+    private void Awake()
     {
-        
+        CreateOrder();
     }
+
+    public void CreateOrder()
+    {
+        Order newOrder = new Order();
+        int orderLength = Random.Range(minItems, maxItems);
+        for(int i = 0; i < orderLength; i++)
+        {
+            Debug.Log($"Box {i+1}/{orderLength}");
+        }
+
+        currentOrder = newOrder;
+    }
+}
+
+public class Order
+{
+    public List<GameObject> OrderRequirements;
+
 }
