6af14eb45a4e0e1a194b6af5ffaa9708f3979277
James-Hicks <james8hicks@gmail.com>
2023-04-29 00:54:07 -0400

    feat(lift): forklift lift movement added

--- LudumDare53/Assets/LD53/Scripts/PlayerControl.cs
+++ LudumDare53/Assets/LD53/Scripts/PlayerControl.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
+    [Header("Lift")]
     [SerializeField] private GameObject Lift;
+    [SerializeField] private Transform LiftBottom;
+    [SerializeField] private Transform LiftTop;
+    [SerializeField] private float liftSpeed = 0.2f;
+
+    [Header("Player")]
     [SerializeField] private float playerSpeed = 3f;
     [SerializeField] private float rotationSpeed = 40f;
 
+    bool liftUp;
+    bool liftDown;
+
+    bool lastLiftUp = true;
+    public float liftPercent;
     private Vector2 playerInput;
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Lift.transform.position = LiftBottom.position;
     }
 
     private void Update()
@@ -27,14 +41,61 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //TODO: If grounded check
         rb.AddForce(transform.forward * playerInput.y * playerSpeed * Time.deltaTime, ForceMode.Impulse);
         transform.Rotate(transform.up, rotationSpeed * playerInput.x * Time.deltaTime);
+
+        if (liftUp)
+        {
+            if (!lastLiftUp)
+            {
+                liftPercent = 1 - liftPercent;
+                lastLiftUp = true;
+            }
+            
+
+            liftPercent += Time.deltaTime * liftSpeed;
+            Lift.transform.position = Vector3.Lerp(LiftBottom.position, LiftTop.position, liftPercent);
+        }
+
+        if (liftDown)
+        {
+            if(lastLiftUp)
+            {
+                liftPercent = 1 - liftPercent;
+                lastLiftUp= false;
+            }
+
+            liftPercent += Time.deltaTime * liftSpeed;
+            Lift.transform.position = Vector3.Lerp(LiftTop.position, LiftBottom.position, liftPercent);
+        }
+        liftPercent = Mathf.Clamp(liftPercent, 0, 1);
     }
 
+    #region InputActions
     private void OnMove(InputValue value)
     {
         playerInput = value.Get<Vector2>();
     }
 
+    private void OnLiftUpPress(InputValue value)
+    {
+        liftUp = true;
+    }
 
+    private void OnLiftUpRelease(InputValue value)
+    {
+        liftUp = false;
+    }
+
+    private void OnLiftDownPress(InputValue value)
+    {
+        liftDown = true;
+    }
+
+    private void OnLiftDownRelease(InputValue value)
+    {
+        liftDown = false;
+    }
+    #endregion
 }
