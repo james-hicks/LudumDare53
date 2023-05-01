6c326697725053761259560cf9553b734eba7dc3
James-Hicks <james8hicks@gmail.com>
2023-05-01 03:05:46 -0400

    Truck sounds done!

--- LudumDare53/Assets/LD53/Scripts/TruckAnimEvents.cs
+++ LudumDare53/Assets/LD53/Scripts/TruckAnimEvents.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TruckAnimEvents : MonoBehaviour
 {
     [SerializeField] private TruckCollider truckCollider;
+    [SerializeField] private AudioSource truckExitSource;
+    [SerializeField] private AudioSource truckEnterSource;
     public void EndLoad()
     {
         truckCollider.EndOrder();
@@ -18,5 +21,33 @@ public class TruckAnimEvents : MonoBehaviour
     public void SwapMaterial()
     {
         truckCollider.SwapMaterial();
+    }
+
+    public void TruckExit()
+    {
+        truckExitSource.volume = 1;
+        truckExitSource.Play();
+        StartCoroutine(StartFade(truckExitSource, 4f, 0f));
+    }
+
+    public void TruckArrive()
+    {
+        truckEnterSource.Play();
+        truckEnterSource.time = 20f;
+        truckEnterSource.volume = 0f;
+        StartCoroutine(StartFade(truckEnterSource, 2f, 1));
+    }
+
+    public IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
